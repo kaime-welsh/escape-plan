1e672fac8383354c2dfcdc00c130807e8fa3e74c
Kaime Welsh <kaime.r.welsh@gmail.com>
2025-12-02 15:34:31 -0800

    Added player class scaffold

---
+++ Player.cs
@@ -0,0 +1,20 @@
+using System.Numerics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+namespace EscapePlan;
+
+class Player
+{
+    Vector3 Position { get; set; }
+    
+    void Update(float dt)
+    {
+        
+    }
+    
+    void Draw()
+    {
+        DrawSphere(Position, 1.0f, Color.Green);
+    }
+}
