48500f8ddcd9939aeea96915c6a241380f589554
Kaime Welsh <kaime.r.welsh@gmail.com>
2025-12-08 19:26:33 -0800

    Once again switch, back to rust with bevy. I'm staying this time okay?

--- src/playing_state.h
+++
@@ -1,9 +0,0 @@
-#ifndef PLAYING_STATE_H
-#define PLAYING_STATE_H
-
-void playing_state_load();
-void playing_state_update();
-void playing_state_render();
-void playing_state_unload();
-
-#endif // PLAYING_STATE_H
